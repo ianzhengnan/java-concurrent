6528227476b8d00637f52284810f1be8d686c998
Ian Zheng <ian.zheng@sap.com>
2018-06-22 12:04:40 +0800

    add annotations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标记线程不安全的类或者写法
+ */
+@Target(ElementType.TYPE) //注解作用范围 类，方法等
+@Retention(RetentionPolicy.SOURCE) //注解存在的范围 Source表示只在编译的时候存在 只是为了让人知道这个类是线程不安全的，
+//如果需要运行时判断这个注解，就要使用RUNTIME
+public @interface NotThreadSafe {
+    String value() default "";
+}
+
